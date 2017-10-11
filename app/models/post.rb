b5c78f8265608dc97c9bf27c20d5421606686156
Yuci <yuci.gou@gmail.com>
2017-10-11 21:32:25 +0000

    Add validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+    # attr_accessor :title, :blurb, :body
+    validates :body, :title, :presence => true
+    validates :blurb, :length => { :in => 10..255 }, :format => { :with => /^\A[a-zA-Z ]+\z/, :message => "Only letters allowed", :multiline => true}
 end
