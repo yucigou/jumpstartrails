70a8bf8c2a763ba5081abd585415b0b70a47d291
Yuci <yuci.gou@gmail.com>
2017-10-11 22:25:45 +0000

    Add unit test cases

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,4 +4,25 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "should not save post without title" do
+    post = Post.new
+    assert !post.save
+  end  
+
+  test "post values should not be empty" do
+    post = Post.new
+    assert post.invalid?
+    assert post.errors[:title].any?
+    assert post.errors[:blurb].any?
+    assert post.errors[:body].any?
+  end  
+  
+  test "blurb should be a string" do
+    post = Post.new(title: posts(:one).title,
+                    blurb: posts(:one).blurb,
+                    body: "Body? Whose?")
+    
+    assert post.title.kind_of? String 
+  end  
 end
