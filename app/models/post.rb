e57c8959c4890a7674c57ad4c983c1b5a12a9b10
Yuci <yuci.gou@gmail.com>
2017-10-11 22:25:13 +0000

    Add comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
     # attr_accessor :title, :blurb, :body
+
+    # Comment out the rules below in order to pass 'rake test'
     validates :body, :title, :presence => true
     validates :blurb, :length => { :in => 10..255 }, :format => { :with => /^\A[a-zA-Z ]+\z/, :message => "Only letters allowed", :multiline => true}
 end
